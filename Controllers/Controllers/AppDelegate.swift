44acabbabf85dc7bb7c823cfb1e5195b5b7c4121
kangddong <rkdehddud96@naver.com>
2023-06-26 02:04:12 +0900

    APNs 추가

--- Controllers/Controllers/AppDelegate.swift
+++ Controllers/Controllers/AppDelegate.swift
@@ -15,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        let _ = GameControllerManager.shared
         
+        UIApplication.shared.registerForRemoteNotifications()
+        UNUserNotificationCenter.current().delegate = self
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            print("Permission granted: \(granted)")
+        }
         return true
     }
 
@@ -33,3 +44,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // registerForRemoteNotifications 에 대한 콜백 두 가지
+    // 장치에 대한 토큰을 가져오지 못한 경우 등록 실패 이류를 설명하는 오류와 함께 해당 메소드가 호출
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("원격 노티피케이션 사용불가 !! : \(error.localizedDescription)")
+        
+    }
+    
+    // 토큰을 받는데 성공한 경우, 이 장치에 알림을 전달할 수 있도록, 해당 토큰을 백엔드 푸시 서버로 보내야함
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
+        print("deviceTokenString: \(deviceTokenString)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // 전달된 페이로드는 알림 컨텐츠의 userInfo 속성에서 추출할 수 있다.
+        let userInfo = response.notification.request.content.userInfo
+        completionHandler()
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        completionHandler(.noData)
+    }
+}
