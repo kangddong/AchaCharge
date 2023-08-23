6dd1dcd1031a701128f72ef008074dec3e79fece
kangddong <rkdehddud96@naver.com>
2023-08-23 23:35:19 +0900

    Programmtically UI로 변경

--- Controllers/Controllers/ViewController.swift
+++ Controllers/Controllers/ViewController.swift
@@ -16,15 +16,71 @@ enum BatteryState: Int {
 
 final class ViewController: UIViewController {
 
-    @IBOutlet weak var gamePadImageView: UIImageView!
-    @IBOutlet weak var batteryStateLabel: UILabel!
-    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-    @IBOutlet weak var loadingView: UIView!
-    @IBOutlet weak var refreshButton: UIButton!
+    // MARK: UI Properties
+    private let indicatorView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.style = .large
+        view.color = .label
+        view.backgroundColor = .systemBackground
+        view.hidesWhenStopped = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     
-    private var circularProgressBarView: CircularProgressBarView!
+    private let loadingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let gamePadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "gamecontroller.fill")
+        imageView.tintColor = .label
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let batteryStateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Not connected.."
+        label.textAlignment = .center
+        label.textColor = .label
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let refreshButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Refresh", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        button.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var circularProgressBarView: CircularProgressBarView = {
+        let view = CircularProgressBarView(frame: .zero)
+        view.tag = _CIRCLEARVIEW_TAG
+        view.isHidden = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     private var circularViewDuration: TimeInterval = 0.5
     
+    // MARK: UI Properties
     private let manager = GameControllerManager.shared
     private let _CIRCLEARVIEW_TAG = 20230701
     
@@ -35,18 +91,19 @@ final class ViewController: UIViewController {
             UserDefaults.shared.setValue(isConnected, forKey: StringKey.CONTROLLER_CONNECTED)
         }
     }
+    
     private var batteryInfo: (level: Float, state: Int) = (0.0, -1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UserDefaults.shared.setValue(false, forKey: StringKey.CONTROLLER_CONNECTED)
-        
-        refreshButton.addTarget(self, action: #selector(tappedRefresh), for: .touchUpInside)
-        
-        addCircularProgressBarView()
+        addSubViews()
+        addConstraints()
         setUpIndicatoreView()
         addControllerObservers()
+        
+        UserDefaults.shared.setValue(false, forKey: StringKey.CONTROLLER_CONNECTED)
+        refreshButton.addTarget(self, action: #selector(tappedRefresh), for: .touchUpInside)
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -83,6 +140,56 @@ final class ViewController: UIViewController {
     }
 }
 
+// MAKR: UI Methods
+extension ViewController {
+    
+    private func addSubViews() {
+        
+        view.backgroundColor = .systemBackground
+        [
+            loadingView,
+            gamePadImageView,
+            circularProgressBarView,
+            batteryStateLabel,
+            refreshButton,
+        ].forEach { view.addSubview($0) }
+        loadingView.addSubview(indicatorView)
+    }
+    
+    private func addConstraints() {
+        
+        NSLayoutConstraint.activate([
+            loadingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            loadingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            loadingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            loadingView.heightAnchor.constraint(equalToConstant: 120),
+            
+            indicatorView.topAnchor.constraint(equalTo: loadingView.topAnchor),
+            indicatorView.centerXAnchor.constraint(equalTo: loadingView.centerXAnchor),
+            indicatorView.widthAnchor.constraint(equalToConstant: 80),
+            indicatorView.heightAnchor.constraint(equalToConstant: 80),
+            
+            circularProgressBarView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            circularProgressBarView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            gamePadImageView.widthAnchor.constraint(equalToConstant: 180),
+            gamePadImageView.heightAnchor.constraint(equalToConstant: 130),
+            gamePadImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gamePadImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            batteryStateLabel.topAnchor.constraint(equalTo: gamePadImageView.bottomAnchor, constant: 36),
+            batteryStateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            refreshButton.topAnchor.constraint(equalTo: batteryStateLabel.bottomAnchor, constant: 57),
+            refreshButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    private func setUpIndicatoreView() {
+     
+        indicatorView.startAnimating()
+    }
+}
 extension ViewController {
 
     private func addControllerObservers() {
@@ -114,23 +221,6 @@ extension ViewController {
         indicatorView.startAnimating()
         manager.getControllerCount()
         updateBatteryInfo()
-    }
-    private func addCircularProgressBarView() {
-        
-        circularProgressBarView = CircularProgressBarView(frame: .zero)
-        circularProgressBarView.tag = _CIRCLEARVIEW_TAG
-        view.addSubview(circularProgressBarView)
-        circularProgressBarView.translatesAutoresizingMaskIntoConstraints = false
-        circularProgressBarView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        circularProgressBarView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        circularProgressBarView.isHidden = true
-    }
-    
-    private func setUpIndicatoreView() {
-     
-        indicatorView.hidesWhenStopped = true
-        indicatorView.startAnimating()
     }
     
     @objc
