e51273a635342fe335b739054bd907c31c99ae34
kangddong <rkdehddud96@naver.com>
2023-08-27 01:47:16 +0900

    StoreKit 연동 !

--- Controllers/Controllers/SettingViewController.swift
+++ Controllers/Controllers/SettingViewController.swift
@@ -7,23 +7,41 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
-
+final class SettingViewController: UIViewController {
+    
+    private lazy var testButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("IAP", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        button.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(tappedTestButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let storKitManager: StoreKitManager = StoreKitManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        print(#fileID, #function)
+        view.addSubview(testButton)
+        NSLayoutConstraint.activate([
+            testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testButton.widthAnchor.constraint(equalToConstant: 100),
+            testButton.heightAnchor.constraint(equalToConstant: 100),
+        ])
         view.backgroundColor = .systemBackground
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    private func tappedTestButton() {
+        print(#function)
+        
+        storKitManager.requestMonthSubscription()
     }
-    */
-
 }
