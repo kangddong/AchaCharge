c819a39be494c1fe5fa0e9da10da6bc0825599a3
kangddong <rkdehddud96@naver.com>
2023-08-28 00:02:21 +0900

    SettingItemCell 추가

---
+++ Controllers/Controllers/Sources/Setting/SettingItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingItemCell.swift
+//  Controllers
+//
+//  Created by 강동영 on 2023/08/28.
+//
+
+import UIKit
+
+class SettingItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
