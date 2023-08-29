9b4f52c771a67f1d14dfb34645dc919de4306543
kangddong <rkdehddud96@naver.com>
2023-08-29 20:17:40 +0900

    IntroViewControlelr 추가

---
+++ Controllers/Controllers/Sources/Setting/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  Controllers
+//
+//  Created by 강동영 on 2023/08/29.
+//
+
+import UIKit
+
+final class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
