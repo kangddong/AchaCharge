529bb41a673fa3cd3ae2cb5bfcfdae7b302f867d
kangddong <rkdehddud96@naver.com>
2023-09-17 01:34:15 +0900

    앱 네임 표시 추가 !

--- Controllers/Controllers/Sources/Models/GameControllerManager.swift
+++ Controllers/Controllers/Sources/Models/GameControllerManager.swift
@@ -137,7 +137,7 @@ class FetchGameControllerOperation: Operation {
         
         let center = UNUserNotificationCenter.current()
         let content = UNMutableNotificationContent()
-        let appName = Bundle.main.infoDictionary["CFBundleDisplayName"] as! String
+        let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as! String
         content.title = appName
         let level = Int(info.batteryLevel * 100)
         content.body = "현재 배터리가 \(level)% 입니다 !"
