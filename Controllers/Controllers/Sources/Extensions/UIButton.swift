64826bd7ac86c6e1bae5f2368db9f93bddde7428
kangddong <rkdehddud96@naver.com>
2023-12-07 00:21:40 +0900

    [ADD] extension of UIButon underline

---
+++ Controllers/Controllers/Sources/Extensions/UIButton.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton.swift
+//  Controllers
+//
+//  Created by 강동영 on 12/7/23.
+//
+
+import UIKit.UIButton
+
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
