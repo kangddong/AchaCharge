bcf4b5d2efa372c41e8b5844a00b552c34d587dd
kangddong <rkdehddud96@naver.com>
2024-05-20 18:05:00 +0900

    mac os 기능 확인

--- Controllers/Controllers-macOS/AppDelegate.swift
+++ Controllers/Controllers-macOS/AppDelegate.swift
@@ -6,15 +6,19 @@
 //
 
 import Cocoa
+import UserNotifications
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private lazy var statusItem: NSStatusItem = {
+        return NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    }()
 
-    
-
+    private let menu = NSMenu()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        requestNotificationAuthorization()
+        setupStatusItem()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,7 +28,62 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
 
+// MARK: - Private Method
+extension AppDelegate {
+    private func requestNotificationAuthorization() {
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            print("Permission granted: \(granted)")
+        }
+    }
+    
+    private func setupStatusItem() {
+        statusItem.button?.imagePosition = .imageTrailing
+        statusItem.button?.image = NSImage(systemSymbolName: "gamecontroller", accessibilityDescription: nil)
+        
+        if #available(macOS 10.15, *) {
+            let font = NSFont.monospacedSystemFont(ofSize: 11, weight: .regular)
+            statusItem.button?.font = font
+        } else {
+            let font = NSFont.monospacedDigitSystemFont(ofSize: 11, weight: .regular)
+            statusItem.button?.font = font
+        }
+        
+        let menuItem1 = NSMenuItem(title: "카카오톡 열기", action: #selector(toggleShowUsage(_:)), keyEquivalent: "s")
+        let menuItem2 = NSMenuItem(title: "모두 읽음 처리", action: #selector(toggleShowUsage(_:)), keyEquivalent: "S")
+        let menuItem3 = NSMenuItem(title: "잠금모드", action: #selector(toggleShowUsage(_:)), keyEquivalent: "")
+        let menuItem4 = NSMenuItem(title: "로그아웃", action: #selector(toggleShowUsage(_:)), keyEquivalent: "")
+        let menuItem5 = NSMenuItem(title: "종료", action: #selector(toggleShowUsage(_:)), keyEquivalent: "")
+        menu.addItem(menuItem1)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(menuItem2)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(menuItem3)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(menuItem4)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(menuItem5)
+//        menu.addItem(withTitle: "Show CPU Usage",
+//                     action: #selector(toggleShowUsage(_:)),
+//                     keyEquivalent: "")
+//        menu.addItem(NSMenuItem.separator())
+//        menu.addItem(withTitle: "About Menubar RunCat",
+//                     action: #selector(toggleShowUsage(_:)),
+//                     keyEquivalent: "")
+//        menu.addItem(withTitle: "Quit Menubar RunCat",
+//                     action: #selector(toggleShowUsage(_:)),
+//                     keyEquivalent: "")
+        statusItem.menu = menu
+    }
+    
+    @objc func toggleShowUsage(_ sender: NSMenuItem) {
+        print(#function)
+    }
+}
--- Controllers/Controllers-macOS/ViewController.swift
+++ Controllers/Controllers-macOS/ViewController.swift
@@ -6,21 +6,64 @@
 //
 
 import Cocoa
+import UserNotifications
 
 class ViewController: NSViewController {
-
+    
+    private let manager = GameControllerManager.shared
+    private var isConnected = false {
+        didSet {
+            isConnectedLabel.stringValue = isConnected ? "Dual Sense\n, Connected" : "Dual Sense\n,DisConnected"
+        }
+    }
+    
+    @IBOutlet weak var isConnectedLabel: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+//        let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as! String
+//
+        let appName = "appName"
+        content.title = appName
+        content.sound = .default
+        let level = Int(0.85 * 100)
+        content.body = "Current Battery is \(level)%"
+        NSLog("filter: Push 성공, 배터리 레벨 : \(level)")
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "batterPush", content: content, trigger: trigger)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5, execute: {
+            print("push sent")
+            center.add(request)
+        })
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+}
 
-
+// MARK: - GameControllerDelegate Method
+extension ViewController: GameControllerDelegate {
+    func didConnectedController() {
+        print(#function, isConnected)
+        isConnected = true
+    }
+    
+    func didDisConnectedController() {
+        print(#function, isConnected)
+        isConnected = false
+    }
 }
 
