93ce39b6b6372e9291eecc425250ee30a9a82793
kangddong <rkdehddud96@naver.com>
2024-05-20 19:27:09 +0900

    [ADD] add ControllerView file

---
+++ Controllers/Controllers/Sources/Controller/ControllerView.swift
@@ -0,0 +1,18 @@
+//
+//  ControllerView.swift
+//  Controllers
+//
+//  Created by 강동영 on 5/20/24.
+//
+
+import UIKit
+
+final class ControllerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
