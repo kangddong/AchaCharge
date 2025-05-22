2e63717ca5f7e88415a99f0236dfd79cdb6087a4
kangddong <rkdehddud96@naver.com>
2025-05-22 23:26:57 +0900

    add: TesterView 추가

---
+++ Controllers/Controllers/Sources/Tester/TesterView.swift
@@ -0,0 +1,18 @@
+//
+//  TesterView.swift
+//  Controllers
+//
+//  Created by 강동영 on 5/22/25.
+//
+
+import SwiftUI
+
+struct TesterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TesterView()
+}
